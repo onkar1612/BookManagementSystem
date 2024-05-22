9d756bcbce44c6f53cf13be4bf24086fe0a8d239
Onkar Nagarkar <onkar1612@gmail.com>
2024-05-22 08:19:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,93 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import Book from './components/Book';
-import AddBook from './components/AddBook';
-import useFetch from './useFetch';
 import Navbar from './components/Navbar';
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
-import About from './components/contactUs';
+import Categories from './Categories';
+import Suppliers from './Suppliers';
+import Home from './Home';
 
 
 
 function App() {
-
-
-    let { data, error } = useFetch('http://localhost:8000/books');
-
-    let [books, setBooks] = useState(null);
-
-    useEffect(()=>{
-        setBooks(data);
-    }, [data]);
-
-
-
-    function handleRemove(id) {
-        fetch(`http://localhost:8000/books/${id}`, {
-            method: 'DELETE'
-        })
-            .then(() => {
-                let newBooks = books.filter(
-                    (element) => {
-                        return element.id !== id;
-                    }
-                )
-                setBooks(newBooks);
-            })
-            .catch(error => {
-                console.error('Error removing book:', error);
-            });
-    }
-
-    function handleSubmit(book) {
-        fetch('http://localhost:8000/books', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(book)
-        })
-            .then(() => {
-                let newBooks = [...books];
-                newBooks.push(book);
-                setBooks(newBooks);
-            })
-            .catch(error => {
-                console.error('Error adding book:', error);
-            });
-    }
 
     return (
         <div id="main-container">
             <Navbar></Navbar>
 
             <Routes>
-                <Route path="/" element={<AddBook handleSubmit={handleSubmit}> </AddBook>}></Route>
-
-                <Route path="/About" element={<About></About>}/>
+                <Route path = "/" element={<Home />} />
+                <Route path ="/Categories" element={<Categories />} />
+                <Route path = "/Suppliers" element={<Suppliers />}/> 
             </Routes>
 
-            
-            {
-                !error ? books &&
-                    books.map(
-                        (element) => {
-                            return <Book
-                                key={element.id}
-                                id={element.id}
-                                title={element.title}
-                                author={element.author}
-                                price={element.price}
-                                handleRemove={handleRemove}
-                                books={books}
-                                setBooks={setBooks}
-                            >
-                            </Book>
-                        }
-                    )
-                    :
-                    error
-            }
+           
         </div>
     );
 }
