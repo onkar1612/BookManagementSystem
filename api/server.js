d9c96713eb9f07f33471f01c956e5f71bca39fd7
Onkar Nagarkar <onkar1612@gmail.com>
2024-05-22 09:56:41 +0530

    Finalize production

--- api/server.js
+++ api/server.js
@@ -1,11 +1,24 @@
-// api/server.js
-const jsonServer = require('json-server')
-const path = require('path')
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, '../data/db.json'))
-const middlewares = jsonServer.defaults()
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("data/db.json");
 
-server.use(middlewares)
-server.use(router)
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
 
-module.exports = server
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to port
+server.listen(3000, () => {
+ console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
